ae0b53b419c51288a4e8b7794f153e56f1f2953b
james65535 <mcafeej@vmware.com>
2018-10-29 10:58:11 +1100

    input validation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 var server string
@@ -23,16 +24,31 @@ func init() {
 	}
 }
 
+// Basic checking on string input
+func sValidation(u string) bool {
+	sCheck, _ := regexp.MatchString("^[a-zA-Z]+$", u)
+	if len(u) < 30 && sCheck {
+		return true
+	}
+	return false
+}
+
 //localhost:8080/?name=bob
 func greeter(w http.ResponseWriter, r *http.Request) {
+	var name string
+	if sValidation(r.URL.Query().Get("name")) {
+		name = r.URL.Query().Get("name")
+	} else {
+		name = "invalid"
+	}
 	log.Println("/ visited by:" + r.RemoteAddr)
-	name := r.URL.Query().Get("name")
 	log.Println("Name:", name)
 	message := "Hello " + name
 	w.Write([]byte(message))
 }
 
 func main() {
+	log.Printf("Starting on %s\n", server)
 	http.HandleFunc("/", greeter)
 	if err := http.ListenAndServe(server, nil); err != nil {
 		panic(err)
