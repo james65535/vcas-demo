b4a447136377d89d93e0d7f0c51372f4b07afa19
james65535 <mcafeej@vmware.com>
2019-01-07 16:27:36 +1100

    html

--- public/test/js/main.jsx
+++ public/test/js/main.jsx
@@ -2,9 +2,6 @@ class Clock extends React.Component {
     render() {
         return (
             <div className="main-container">
-                <div className="alert alert-app-level">
-                    Alert Pane
-                </div>
                 <header className="header header-6">
                     <div _ngcontent-c0="" className="branding">
                         <a _ngcontent-c0="" className="nav-link" href="#">
@@ -26,9 +23,6 @@ class Clock extends React.Component {
                     <div _ngcontent-c0="" className="header-actions">
                     </div>
                 </header>
-                <nav className="subnav">
-                    Subnav Pane
-                </nav>
                 <div className="content-container">
                     <div className="content-area">
                         <div>
@@ -36,9 +30,6 @@ class Clock extends React.Component {
                             <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
                         </div>
                     </div>
-                    <nav className="sidenav">
-                        Sidenav
-                    </nav>
                 </div>
             </div>
         );
