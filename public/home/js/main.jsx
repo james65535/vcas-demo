163ca4dca0076fd2e3503e0d957eb9b3927d6c7c
james65535 <mcafeej@vmware.com>
2019-01-15 16:37:40 +0800

    demo1

--- public/home/js/main.jsx
+++ public/home/js/main.jsx
@@ -26,7 +26,7 @@ class Clock extends React.Component {
                 <div className="content-container">
                     <div className="content-area">
                         <div>
-                            <h1>Hello, taiwan offsite team</h1>
+                            <h1>Hello, taiwan offsite team, whatever</h1>
                             <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
                         </div>
                     </div>
