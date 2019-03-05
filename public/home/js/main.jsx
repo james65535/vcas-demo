ddeb4deda34da133f94db37343a0e9f2c8f5c5fc
davlloyd <davidlloyd0@gmail.com>
2019-03-04 17:22:04 -0800

    blah

--- public/home/js/main.jsx
+++ public/home/js/main.jsx
@@ -30,12 +30,12 @@ class Clock extends React.Component {
                         <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                             <a href="..." class="card clickable">
                                 <div class="card-img">
-                                    <img src="/images/apple.png">
+                                    <img src="/images/apple.png" />
                                 </div>
                                 <div class="card-block">
                                     <p class="card-text">
                                     Platform: {navigator.platform}
-                                    <br /> 
+                                    <br />  
                                     UserAgent: {navigator.userAgent}
                                     <br />
                                     Geo: {navigator.geolocation.getCurrentPosition.name}
