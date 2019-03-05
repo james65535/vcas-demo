091a735796ef01f50c693026ec4456bd4016866f
james65535 <mcafeej@vmware.com>
2019-03-05 14:49:34 -0800

    rollback

--- public/home/js/main.jsx
+++ public/home/js/main.jsx
@@ -25,44 +25,7 @@ class Clock extends React.Component {
                 </header>
                 <div className="content-container">
                     <div className="content-area">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-                                <a href="..." class="card clickable">
-                                    <div class="card-img">
-                                        <img id="platformimage" src="/images/unicorn.png" />
-                                    </div>
-                                    <div class="card-block">
-                                        <p class="card-text">
-                                            <table class="table table-vertical">
-                                                <tbody>
-                                                    <tr>
-                                                        <th>Platform</th>
-                                                        <td>{navigator.platform}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Browser Vendor</th>    
-                                                        <td>{navigator.vendor}</td> 
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Browser Type</th>
-                                                        <td>{navigator.product}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Browser Name</th>
-                                                        <td>{navigator.appName}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>Browser Version</th>
-                                                        <td>{navigator.appVersion}</td>
-                                                    </tr>
-                                                </tbody>
-
-                                            </table>
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
+                        todo
                     </div>
                 </div>
             </div>
@@ -75,39 +38,6 @@ function tick() {
         <Clock date={new Date()} />,
         document.getElementById('root')
     );
-
-    getPlatformImage();
 }
-
-function getPlatformImage() {
-    var $img = document.getElementById("platformimage")
-    switch(navigator.platform) {
-        case "MacIntel":
-            $img.src = "/images/apple.png"
-            break;
-        case "Win32":
-            $img.src = "/images/windows.png"
-            break;
-        case "Linux armv8l":
-            $img.src = "/images/android.png"
-            break;
-        case "iPhone":
-            $img.src = "/images/ios.png"
-            break;
-        default:
-            $img.src = "/images/unicorn.png"
-       break;
-    }
-    if (document.body.clientWidth > 300 ){
-        $img.style.width = "250px"
-        $img.style.height = "250px"
-    } else {
-        $img.style.width = "100px"
-        $img.style.height = "100px"
-    }
-
-    $img.align = "middle"    
-}
-
 
 setInterval(tick, 1000);
