8a688810f2f4b9855dda3935083f2de92c27281b
davlloyd <davidlloyd0@gmail.com>
2019-03-05 13:02:23 -0800

    table width remove

--- public/home/js/main.jsx
+++ public/home/js/main.jsx
@@ -29,7 +29,7 @@ class Clock extends React.Component {
                             <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                                 <a href="..." class="card clickable">
                                     <div class="card-img">
-                                        <table width="250px">
+                                        <table>
                                          <tr>
                                              <td>
                                                  <img id="platformimage" src="/images/unicorn.png" />
