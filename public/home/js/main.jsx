0e38af1b13cd7d3b05ea2b8ab18f37ecaa923d54
james65535 <mcafeej@vmware.com>
2019-03-05 00:02:44 -0800

    geo adjustment

--- public/home/js/main.jsx
+++ public/home/js/main.jsx
@@ -37,7 +37,7 @@ class Clock extends React.Component {
                                             <br />
                                             User Agent: {navigator.userAgent}
                                             <br />
-                                            Geo: {navigator.geolocation.getCurrentPosition()}
+                                            Geo: {navigator.geolocation}
                                             <br />
                                             Platform: {navigator.platform}
                                             <br />
